9dbffdbd09c853d9d28b0a73649c07242f8959fe
Vishnu <220510404+Vishnucseai@users.noreply.github.com>
2026-09-07 11:37:04 +0530

    Remove BasicPrograms from Strings branch

--- BasicPrograms/Miles10.java
+++
@@ -1,7 +0,0 @@
-class Miles{
-	public static void main(String[] args) {
-	    double kilometers = 20;
-	    double Miles = kilometers * 0.621371;
-		System.out.println("Miles = kilometers * 0.621371 : " + Miles);
-	}
-}
