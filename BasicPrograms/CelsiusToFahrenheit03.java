934d3e1d5b82c5256a43d5fda40b67535e528318
Vishnu <220510404+Vishnucseai@users.noreply.github.com>
2026-09-07 17:02:10 +0530

    add searching and sorting problems

--- BasicPrograms/CelsiusToFahrenheit03.java
+++
@@ -1,7 +0,0 @@
-public class CelsiusToFahrenheit{
-	public static void main(String[] args) {
-	    double Celsius = 30.0;
-	    double Fahrenheit = (Celsius * 9/5) + 32;
-		System.out.println("Fahrenheit = " + Fahrenheit);
-	}
-}
